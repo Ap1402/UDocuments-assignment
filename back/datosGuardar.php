3c2f5c0d9cb054cf965cd0dfc281c68174aeafa7
Andres <andres.pintosantiso@gmail.com>
2019-04-14 13:16:04 -0400

    Act crud guardar

--- back/datosGuardar.php
+++ back/datosGuardar.php
@@ -5,12 +5,7 @@ $errores = array();
 $datos = array();
 
 //Tomando datos y eliminado espacios en blanco final e inicio.
-$p_nombre = filter_var($_POST['p_nombre'], FILTER_SANITIZE_STRING);
-$s_nombre = trim($_POST['s_nombre']);
-$p_apellido = trim($_POST['p_apellido']);
-$s_apellido = trim($_POST['s_apellido']);
 $cedula = trim($_POST['cedula']);
-$correo = trim($_POST['correo']);
 $estado_civil = filter_var($_POST['estado_civil'], FILTER_SANITIZE_NUMBER_INT);
 
 //nacimiento
@@ -20,12 +15,11 @@ $estado_nac = filter_var($_POST['estado'], FILTER_SANITIZE_STRING);
 $ciudad_nac = filter_var($_POST['ciudad'], FILTER_SANITIZE_STRING);
 $municipio_nac = filter_var($_POST['municipio'], FILTER_SANITIZE_STRING);
 
-//etnia
-$p_nombre = filter_var($_POST['p_nombre'], FILTER_SANITIZE_STRING);
-$p_nombre = filter_var($_POST['p_nombre'], FILTER_SANITIZE_STRING);
-
+//discapacidad
+$discapacidad = filter_var($_POST['tipo_discapacidad'], FILTER_SANITIZE_STRING);
 
 //turno y carrera
+
 $turno = filter_var($_POST['turno'], FILTER_SANITIZE_NUMBER_INT);
 $carrera = filter_var($_POST['carrera'], FILTER_SANITIZE_STRING);
 
@@ -44,13 +38,28 @@ $mov_tel = filter_var($_POST['movil'], FILTER_SANITIZE_NUMBER_INT);
 $trab_tel = filter_var($_POST['trabajo'], FILTER_SANITIZE_NUMBER_INT);
 
 
-// 
+// Dirección trabajo
+
+$postal_trabajo = filter_var($_POST['t_postal'], FILTER_SANITIZE_NUMBER_INT);
+$estado_trabajo = filter_var($_POST['t_estado'], FILTER_SANITIZE_STRING);
+$ciudad_trabajo = filter_var($_POST['t_ciudad'], FILTER_SANITIZE_STRING);
+$municipio_trabajo = filter_var($_POST['t_municipio'], FILTER_SANITIZE_STRING);
+
+// emergencia
+
+$e_nombre = filter_var($_POST['e_nombre'], FILTER_SANITIZE_STRING);
+$e_hab_tel = filter_var($_POST['e_local'], FILTER_SANITIZE_NUMBER_INT);
+$e_mov_tel = filter_var($_POST['e_movil'], FILTER_SANITIZE_NUMBER_INT);
+$parentesco = filter_var($_POST['parentesco'], FILTER_SANITIZE_STRING);
+
+//Datos titulo
+$i_nombre = filter_var($_POST['i_nombre'], FILTER_SANITIZE_STRING);
+$i_egreso = filter_var($_POST['i_egreso'], FILTER_SANITIZE_NUMBER_INT);
+$i_codigo = filter_var($_POST['i_codigo'], FILTER_SANITIZE_NUMBER_INT);
+$i_estado = filter_var($_POST['i_estado'], FILTER_SANITIZE_STRING);
+$tipo_inst = filter_var($_POST['tipo_inst'], FILTER_SANITIZE_NUMBER_INT);
 
 
-$username = trim($_POST['username']);
-
-$contrasena = trim($_POST['contrasena']);
-$contrasena = password_hash($contrasena, PASSWORD_DEFAULT);
 
 // A decidir si se prohibira el registro a cedulas repetidas o a usuarios
 $sql = "SELECT * FROM  alumnos WHERE cedula='$cedula'";
